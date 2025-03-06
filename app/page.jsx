aeb8b3457a88c285482c2730ae227c5742390c64
Ria Arora <riaarora@Rias-MacBook-Air.local>
2025-03-06 22:45:16 +0530

    add post mobile view changes

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,7 @@ async function Page() {
       </Head>
       <div className='h-[100vh] bg-gradient-to-b ' style={{ backgroundImage: "url('/pink.avif')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
         <div className='w-screen h-[90vh] flex flex-col items-center bg-gradient-to-b'>
-          <header className='w-full lg:h-[15vh] md:h-[15vh] h-[20vh] shadow-zinc-100 shadow-md flex justify-center items-center px-12 gap-1 flex-col'>
+          <header className='w-full lg:h-[15vh] md:h-[15vh] h-[15vh] shadow-zinc-100 shadow-md flex justify-center items-center px-12 gap-1 flex-col'>
             <div className='flex flex-col justify-center items-center '>
               <h1 className='text-4xl font-bold sm:text-4xl md:text-5xl lg:text-5xl'>CBS Confessions</h1>
               <p className='text-lg md:text-xl lg:text-xl mt-2'>Safe space for your daily vents</p>
@@ -43,11 +43,12 @@ async function Page() {
                 <UserButton />
               </SignedIn>
             </div>
-            <div className='md:hidden lg:hidden flex justify-center items-center'>
-            <Link href={'/create'} className='border  text-blue-950 bg-white rounded-full text-xl font-semibold flex justify-center items-center m-2 p-2 py-1'> Add post </Link>
-            </div>
+            
           </header>
-          <div className='w-screen h-[75vh] flex-2 overflow-y-auto lg:w-2/3 p-4 flex flex-col items-center scrollbar-hide mt-6'>
+          <div className='w-screen h-[75vh] flex-2 overflow-y-auto lg:w-2/3 p-4 flex flex-col items-center scrollbar-hide mt-4'>
+            <div className='md:hidden lg:hidden flex justify-center items-center'>
+              <Link href={'/create'} className='border  text-blue-950 bg-white rounded-full text-xl font-semibold flex justify-center items-center mb-2 p-2 py-1'> Add post </Link>
+            </div>
             {formattedPosts.map((post, index) => (
               <Card
                 key={index}
